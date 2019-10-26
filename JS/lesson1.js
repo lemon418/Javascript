eac37a4fa4395f91b669322530aa0ab05f1cf204
jerzhy <grenngreen80@gmail.com>
2019-10-26 12:54:10 +0200

    well done

--- JS/lesson1.js
+++ JS/lesson1.js
@@ -2,6 +2,14 @@ let num = 4;
 result = num & 1 ? "Нечетное число" : "Четное число";
 console.log(result);
 
+let num2 = 4;
+result2 = num2 % 2 == 0 ? "Нечетное число" : "Четное число";
+console.log(result2);
+
+let num3 = 9;
+let num4 = 8;
+result3 = Math.abs(10 - num3) < Math.abs(10 - num4) ?  "Первое число ближе к 10" : "Второе число ближе к 10";
+console.log(result3);
 
 width = 50;
 height =100;
