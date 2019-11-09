ca79e6854e093e7a73585caae522695d2c5e4514
jerzhy <grenngreen80@gmail.com>
2019-11-10 01:37:29 +0200

    strings

--- JS/lesson4_HW.js
+++ JS/lesson4_HW.js
@@ -7,6 +7,44 @@ function alph(someStr) {
 }
 alph(str);
 
+
+
 // Задача №2
 
+let str2 = prompt();
+let strSub = prompt();
 
+let sum = str2.indexOf(strSub);
+
+
+if (sum === -1) {
+	alert("Ничего не найдено");
+}
+
+else if (sum >= 0) {
+	alert("Совпадение присутствует");
+}
+
+
+// Задача №3
+
+let str4 = "пяяgяяп"; 
+
+function ifPolindrom(ifPol) {
+	let mid = ifPol.length / 2;
+	let strB =  ifPol.slice(0 , -mid);
+	let strC = ifPol.slice(-mid - 0.5, ifPol.length);
+	let arrC = strC.split("");  //превращаем одну из строк в массив
+	arrC.reverse(); 			// реверсируем массив
+	let revStrC = arrC.join(""); // склеиваем реверсный массив в строку
+	// console.log(revStrC);
+	// console.log(strB);
+	 if (strB == revStrC) {
+ 		 console.log("Строка является полиндромом");
+	}
+	 else if (strB !== revStrC) {
+	 	 console.log("Строка не является полиндромом");
+	 }
+}
+
+ifPolindrom(str4);
