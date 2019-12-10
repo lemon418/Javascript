26765e8ace57f713352488c1ee270c89722cf94a
jerzhy <grenngreen80@gmail.com>
2019-12-10 02:02:57 +0200

    lesson8_HW

---
+++ JS/lesson8_HW.js
@@ -0,0 +1,84 @@
+// Задача №1
+
+let canvas = document
+    .getElementsByTagName("canvas")[0];
+
+canvas.width = 800;
+canvas.height = 600;
+
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#99ccff";
+ctx.fillRect(0, 0, 800, 600);
+
+
+let rabbit = {
+    img: 'rabbit.png',
+    x: null,
+    y: null,
+    width: 128,
+    height: 128
+};
+
+function drawImg(obj) {
+    let img = new Image();
+    img.src = 'img/' + obj.img;
+    img.onload = () => {
+        ctx.drawImage(
+            img,
+            obj.x = Math.floor((Math.random() * 672) +1),
+            obj.y = Math.floor((Math.random() * 472) +1),
+            obj.width, obj.height
+        );
+    }
+}
+
+drawImg(rabbit);
+drawImg(rabbit);
+
+let tiger = {
+    img: 'tiger.png',
+    x: 22,
+    y: 15,
+    width: 128,
+    height: 128
+};
+
+drawImg(tiger);
+
+function move(obj, event) {
+    // w - перемещение вверх
+    // a - перемещение влево
+    // d - перемещение вправо
+    // s - перемещение вниз
+    console.log(event.code);
+    if (event.code === "KeyD") {
+        console.log("перемещение вправо");
+        clearImg(obj);
+        obj.x += 2;
+        drawImg(obj);
+    } else if (event.code === "KeyA") {
+        console.log("перемещение влево");
+        clearImg(obj);
+        obj.x -= 2;
+        drawImg(obj);
+    } else if (event.code === "KeyW") {
+        console.log("перемещение вверх");
+        clearImg(obj);
+        obj.y -= 2;
+        drawImg(obj);
+    } else if (event.code === "KeyS") {
+        console.log("перемещение вниз");
+        clearImg(obj);
+        obj.y += 2;
+        drawImg(obj);
+    }
+}
+
+function clearImg(obj) {
+    ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+}
+
+document.addEventListener("keydown",
+    move.bind(document, tiger));
+
+
